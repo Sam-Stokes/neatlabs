e4a436b88fce5d8639bd6b45b370d67707aa8aed
Sam Stokes <sam@stellarlibrary.com>
2023-12-27 16:00:44 +1300

    initial commit

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header: React.FC = () => {
+    return (
+        <div>
+            <a href="/">
+                <img className="headerLogo" src="/src/assets/logo-white.png" alt="logo-white" />
+            </a>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/config/cursorConfig.ts
@@ -0,0 +1,10 @@
+const animatedCursorConfig = {
+    color: '0, 90, 90',
+    outerAlpha: 0.3,
+    innerSize: 8,
+    outerSize: 24,
+    outerScale: 2,
+    trailingSpeed: 8,
+}
+
+export default animatedCursorConfig
