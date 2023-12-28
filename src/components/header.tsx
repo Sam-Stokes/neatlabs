ff539f617d78df49261884fc4af5fba6bcac5891
Sam Stokes <sam@stellarlibrary.com>
2023-12-29 12:46:23 +1300

    more

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,43 +7,15 @@ const Header: React.FC = () => {
     useEffect(() => {
         const tl = gsap.timeline()
 
-        let xStart, yStart, xMove, yMove, scaleSize
-
-        // Adjustments based on screen width
-        if (window.innerWidth > 768) {
-            // For larger screens
-            xStart = '0' // Starting X position
-            yStart = '-5%' // Starting Y position
-            xMove = '-42%'
-            yMove = '-40%'
-            scaleSize = 0.1
-        } else {
-            // For smaller screens
-            xStart = '0' // Starting X position
-            yStart = '100%' // Starting Y position
-            xMove = '0' // Less movement
-            yMove = '-30%'
-            scaleSize = 0.25 // Larger scale
-        }
-
-        // Set initial position and scale
-        gsap.set('.headerLogo', {
-            scale: window.innerWidth > 768 ? 0.8 : 1, // Conditional starting scale
-            x: xStart,
-            y: yStart,
-        })
-
         // Wait for 2 seconds with the logo large
-        tl.to('.headerLogo', {
-            duration: 2, // Wait for 2 seconds
-        })
+        tl.to('.headerLogo', { duration: 2 })
 
         // Then shrink the logo and move it
         tl.to('.headerLogo', {
-            scale: scaleSize,
-            x: xMove,
-            y: yMove,
-            duration: 2, // Duration of the animation
+            scale: window.innerWidth > 768 ? 0.1 : 0.35,
+            x: window.innerWidth > 768 ? '-45%' : '0',
+            y: window.innerWidth > 768 ? '-45%' : '-45%',
+            duration: 2,
             ease: 'power3.out',
         })
     }, [])
