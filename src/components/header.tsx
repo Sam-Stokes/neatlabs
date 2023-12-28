443186db7194a273dfd61d5821993fe539436444
Sam Stokes <sam@stellarlibrary.com>
2023-12-29 10:50:02 +1300

    more logo styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,22 +13,22 @@ const Header: React.FC = () => {
         if (window.innerWidth > 768) {
             // For larger screens
             xStart = '0' // Starting X position
-            yStart = '-15%' // Starting Y position
+            yStart = '-5%' // Starting Y position
             xMove = '-42%'
-            yMove = '-44%'
+            yMove = '-40%'
             scaleSize = 0.1
         } else {
             // For smaller screens
             xStart = '0' // Starting X position
-            yStart = '30%' // Starting Y position
+            yStart = '100%' // Starting Y position
             xMove = '0' // Less movement
-            yMove = '-35%'
+            yMove = '-30%'
             scaleSize = 0.25 // Larger scale
         }
 
         // Set initial position and scale
         gsap.set('.headerLogo', {
-            scale: 1, // Starting scale
+            scale: window.innerWidth > 768 ? 0.8 : 1, // Conditional starting scale
             x: xStart,
             y: yStart,
         })
@@ -50,9 +50,7 @@ const Header: React.FC = () => {
 
     return (
         <div className="headerContainer">
-            <a href="/">
-                <img className="headerLogo" src="/src/assets/logo-white.png" alt="logo-white" />
-            </a>
+            <img className="headerLogo" src="/src/assets/logo-white-cropped.png" alt="logo-white" />
         </div>
     )
 }
