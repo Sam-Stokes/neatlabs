06b52f10d3618a3b4f72d1cbc005f679d550d5da
Sam Stokes <sam@stellarlibrary.com>
2023-12-29 16:35:23 +1300

    adding astronaut dude

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useLenis } from '@studio-freight/react-lenis'
 import './styles/app.scss'
 import AnimatedCursor from 'react-animated-cursor'
 import Header from './components/header'
+import Hero from './components/hero'
 
 // https://www.npmjs.com/package/react-animated-cursor#-commands
 
@@ -12,6 +13,7 @@ function App() {
         <>
             <AnimatedCursor innerSize={4} outerSize={4} color="20, 255, 75" trailingSpeed={1} />
             <Header />
+            <Hero />
         </>
     )
 }
